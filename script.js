117ee37287e1ed300b6028843972e2094949fa92
ryandejaegher <dejaegher.ryan@gmail.com>
2021-05-18 14:12:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,16 @@
 	
     var sections = document.querySelectorAll('section');
     sections.forEach(section => {
+
       var rows = section.querySelectorAll('.row');  
+      var images = section.querySelectorAll('.image-block');
+
       rows.forEach((row,i) => {
         row.classList.add(`x-row-${i+1}`);
+      })
+
+      images.forEach((image,i) => {
+        image.classList.add(`image-${i+1}`);
       })
     })
 })
